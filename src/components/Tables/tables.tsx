d5959b75c011de2abbe2ac075b31c4397e8ec171
Stefan Wright <stefanwright1988@gmail.com>
2021-08-05 06:32:55 +0100

    flexbox version of table started

--- src/components/Tables/tables.tsx
+++ src/components/Tables/tables.tsx
@@ -6,60 +6,87 @@ import { CovidInfo } from "../../interfaces/covidInterface";
 interface CountriesTableProps {
   tableData: CovidInfo[];
 }
-const FullWidthTable = styled.table`
-  width: 100%;
-  border-collapse: collapse;
-  th {
-    background-color: #ddd;
-    position: sticky;
-    top: 0;
-    z-index: 9999;
-  }
-  tr {
-    &:nth-child(even) {
-      background: mediumseagreen;
-    }
 
-    &:nth-child(odd) {
-      background: royalblue;
-    }
+const TableContainer = styled.div`
+  display: block;
+  margin: 2em auto;
+  width: 90%;
+  max-width: 900px;
+  div {
+    box-sizing: border-box;
   }
 `;
-const FullWidthTableContainer = styled.div`
-  height: 400px;
-  overflow: auto;
-  width: 100%;
+
+const TableHeader = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  border-left: solid 1px #d9d9d9;
+  transition: 0.5s;
 `;
+
+const TableRow = styled.div`
+  width: calc(100% / 6);
+  text-align: center;
+  padding: 0.5em 0.5em;
+  border-right: solid 1px #d9d9d9;
+  border-bottom: solid 1px #d9d9d9; ;
+`;
+
 export const CountriesTable = (props: CountriesTableProps) => {
   return (
-    <FullWidthTableContainer>
-      <FullWidthTable>
-        <thead>
-          <th>Country</th>
-          <th>Cases</th>
-          <th>Cases %</th>
-          <th>Deaths</th>
-          <th>Recovered</th>
-          <th>Active</th>
-        </thead>
-        <tbody>
-          {props?.tableData.map(
-            (
-              { country, population, cases, deaths, recovered, active },
-              index: number
-            ) => (
-              <tr key={index}>
-                <td>{country}</td>
-                <td>{Number(cases).toLocaleString()}</td>
-                <td>{percentage(Number(cases), Number(population))}</td>
-                <td>{Number(deaths).toLocaleString()}</td>
-                <td>{Number(recovered).toLocaleString()}</td>
-                <td>{Number(active).toLocaleString()}</td>
-              </tr>
-            )
-          )}
-        </tbody>
-      </FullWidthTable>
-    </FullWidthTableContainer>
+    <TableContainer
+      className="table-container"
+      role="table"
+      aria-label="Destinations"
+    >
+      <TableHeader className="flex-table header" role="rowgroup">
+        <TableRow className="flex-row first" role="columnheader">
+          Country
+        </TableRow>
+        <TableRow className="flex-row" role="columnheader">
+          Cases
+        </TableRow>
+        <TableRow className="flex-row" role="columnheader">
+          Cases %
+        </TableRow>
+        <TableRow className="flex-row" role="columnheader">
+          Deaths
+        </TableRow>
+        <TableRow className="flex-row" role="columnheader">
+          Recovered
+        </TableRow>
+        <TableRow className="flex-row" role="columnheader">
+          Active
+        </TableRow>
+      </TableHeader>
+      {props?.tableData.map(
+        (
+          { country, population, cases, deaths, recovered, active },
+          index: number
+        ) => (
+          <div className="flex-table row" role="rowgroup" key={index}>
+            <div className="flex-row first" role="cell">
+              <span className="flag-icon flag-icon-gb"></span>
+              {country}
+            </div>
+            <div className="flex-row" role="cell">
+              {Number(cases).toLocaleString()}
+            </div>
+            <div className="flex-row" role="cell">
+              {percentage(Number(cases), Number(population))}
+            </div>
+            <div className="flex-row" role="cell">
+              {Number(deaths).toLocaleString()}
+            </div>
+            <div className="flex-row" role="cell">
+              {Number(recovered).toLocaleString()}
+            </div>
+            <div className="flex-row" role="cell">
+              {Number(active).toLocaleString()}
+            </div>
+          </div>
+        )
+      )}
+    </TableContainer>
   );
 };
